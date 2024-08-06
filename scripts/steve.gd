916df2e08121641172bda1cc4bc8c122ea1a1fd3
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2024-08-06 11:08:55 -0300

    Rolling

--- scripts/steve.gd
+++ scripts/steve.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+const SPEED = 6
+const ROTATION = 7
+
 func _ready():
 	pass
 
@@ -7,18 +10,22 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 5
+		velocity.x = SPEED
+		$MeshInstance3D.rotate_z(deg_to_rad(-ROTATION))
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -5
+		velocity.x = -SPEED
+		$MeshInstance3D.rotate_z(deg_to_rad(ROTATION))
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.1)  # Adjusted the interpolation factor for smoother deceleration
 		
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = -5
+		velocity.z = -SPEED
+		$MeshInstance3D.rotate_x(deg_to_rad(-ROTATION))
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = 5
+		velocity.z = SPEED
+		$MeshInstance3D.rotate_x(deg_to_rad(ROTATION))
 	else:
 		velocity.z = lerp(velocity.z, 0.0, 0.1)  # Adjusted the interpolation factor for smoother deceleration
 
